48cc436503f4c805a8d007d7ae14fc5940a420ac
ofigensky <bulletproofgleb@icloud.com>
2022-06-16 14:48:57 +0300

    Initial Commit

---
+++ JustMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustMap
+//
+//  Created by Gleb Glushok on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
